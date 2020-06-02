8da7d1411e8ca95a8d6e458bc1db7a16d6bc65d0
Roman Imankulov <roman.imankulov@gmail.com>
2020-06-02 19:35:49 +0100

    Implement formatted_dict() and extend README

--- stripe_repr/repr.py
+++ stripe_repr/repr.py
@@ -1,6 +1,7 @@
 import datetime
 
-import stripe
+from stripe import ListObject
+from stripe.stripe_object import StripeObject
 
 
 def format_stripe(obj):
@@ -13,38 +14,39 @@ def format_stripe(obj):
     - Everything else usually have an ID, and can be identified as such. If possible,
       we also set the name.
     """
-    if type(obj) == stripe.stripe_object.StripeObject:
+    if type(obj) == StripeObject:
         return format_value_object(obj)
-    elif "data" in obj:
+    elif type(obj) == ListObject:
         return format_collection(obj)
     else:
         return format_entity(obj)
 
 
-orig_repr = stripe.stripe_object.StripeObject.__repr__
+orig_repr = StripeObject.__repr__
 
 
 def patch():
     """Patch __repr__ of Stripe objects for a more compact representation."""
-    stripe.stripe_object.StripeObject.__repr__ = format_stripe
+    StripeObject.__repr__ = format_stripe
+    StripeObject.formatted_dict = formatted_dict
+    StripeObject.d = formatted_dict
 
 
 def unpatch():
     """Unpatch __repr__ of Stripe object."""
-    stripe.stripe_object.StripeObject.__repr__ = orig_repr
+    StripeObject.__repr__ = orig_repr
+    try:
+        delattr(StripeObject, "formatted_dict")
+        delattr(StripeObject, "d")
+    except AttributeError:
+        pass
 
 
 def format_value_object(obj):
     """Helper function to format objects without IDs."""
     name = obj.__class__.__name__
-    args = dict(obj).items()
-    formatted_args = ", ".join(
-        [
-            "{}={!r}".format(k, stripe_modify_integers(v))
-            for k, v in args
-            if not is_empty(v)
-        ]
-    )
+    compact_obj = formatted_dict(obj)
+    formatted_args = ", ".join(["{}={!r}".format(k, v) for k, v in compact_obj.items()])
     return "{}({})".format(name, formatted_args)
 
 
@@ -55,13 +57,8 @@ def is_empty(value):
 def format_collection(obj):
     """Helper function to format collections."""
     name = obj.__class__.__name__
-    args = [
-        ("data", obj.get("data")),
-    ]
-    formatted_args = ", ".join(
-        ["{}={!r}".format(k, v) for k, v in args if v is not None]
-    )
-    return "{}({})".format(name, formatted_args)
+    data = obj.get("data")
+    return "{}(data={})".format(name, data)
 
 
 def format_entity(obj):
@@ -70,6 +67,7 @@ def format_entity(obj):
     args = [
         ("id", obj.get("id")),
         ("name", obj.get("name")),
+        ("name", obj.get("description")),
     ]
     formatted_args = ", ".join(
         ["{}={!r}".format(k, v) for k, v in args if v is not None]
@@ -77,7 +75,17 @@ def format_entity(obj):
     return "{}({})".format(name, formatted_args)
 
 
-def stripe_modify_integers(value):
+def formatted_dict(obj):
+    """
+    Helper function to provide a readabale dict.
+
+    The function replaces integers that look like timestamps, with the real timestamp
+    objects. Also, it doesn't records with None and [] to the output.
+    """
+    return {k: guess_datetimes(v) for k, v in dict(obj).items() if not is_empty(v)}
+
+
+def guess_datetimes(value):
     """Helper function that formats integers as datetime objects."""
     min_ts = ts(2010, 1, 1)
     max_ts = ts(2030, 1, 1)
--- tests/test_stripe_repr.py
+++ tests/test_stripe_repr.py
@@ -7,24 +7,41 @@ patch()
 
 
 def test_entity():
+    # entity shows the id and the name.
     customer = stripe.Customer(id="cust_foo")
     customer.update({"name": "Foo"})
     assert repr(customer) == "Customer(id='cust_foo', name='Foo')"
 
 
 def test_value_object():
+    # value object shows the entire contents.
     obj = stripe.stripe_object.StripeObject(id=None)
     obj.update({"name": "Foo"})
     assert repr(obj) == "StripeObject(name='Foo')"
 
 
 def test_value_object_datetime():
+    # value object converts integers to date strings.
     obj = stripe.stripe_object.StripeObject(id=None)
     obj.update({"created": 1577836800})
     assert repr(obj) == "StripeObject(created='2020-01-01T00:00:00')"
 
 
 def test_collection():
-    obj = stripe.Invoice(id=None)
-    obj.update({"data": [stripe.InvoiceItem(id="foo_1")]})
-    assert repr(obj) == "Invoice(data=[InvoiceItem(id='foo_1')])"
+    # collection returns only its data
+    customers = stripe.ListObject()
+    customers.update(
+        {"data": [stripe.Customer(id="cust_foo"), stripe.Customer(id="cust_bar")]}
+    )
+    expected_repr = (
+        "ListObject(data=[Customer(id='cust_foo'), Customer(id='cust_bar')])"
+    )
+    assert repr(customers) == expected_repr
+
+
+def test_d():
+    # Formatted dict (also know as "d") removes None and [], and converts integers to
+    # date strings.
+    obj = stripe.stripe_object.StripeObject(id=None)
+    obj.update({"name": "Foo", "created": 1577836800, "meta_info": None})
+    assert obj.d() == {"name": "Foo", "created": "2020-01-01T00:00:00"}
